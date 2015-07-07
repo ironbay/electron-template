0408ff9b39243a98548e91095672d62489d73c4d
Dax Ravi <d@ironbay.digital>
2015-07-07 16:52:03 -0400

    MAde changes to support electron

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+Install Dependencies:
+`npm install`
+
 Launch Development Server: 
 `npm run debug`
 
+Start Electron:
+`electron .`
+
+
 Build For Production:
 `npm run build`
+
+Note you must modify /electron/main.js to point to 'prod.html' so it looks for static asset files instead of your webpack dev server.  TODO: Make this process automatic
+
+
--- app/main.js
+++ app/main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Router from 'react-router';
-import BrowserHistory from 'react-router/lib/BrowserHistory';
+import HashHistory from 'react-router/lib/HashHistory';
 import routes from './routes';
 import { createRedux } from 'redux';
 import * as stores from "stores"
 import { Provider } from 'redux/react';
 import Root from "root"
 
-const history = new BrowserHistory();
+const history = new HashHistory();
 const redux = createRedux(stores)
 const element = (
   <Provider redux={redux}>
